2a88bba834ebccfd1a687f52711db6c2e1f27d55
juan pablo <chito.pepsi@gmail.com>
2023-04-16 13:48:46 -0600

    Pagina contacto con validaciones de formulario y navbar mejorado

--- js/main.js
+++ js/main.js
@@ -1,147 +1,47 @@
-const $navIndex = document.getElementById("navIndex");
-console.log($navIndex);
-const $navBarContacto = document.getElementById("navBarContacto");
-let navBarIndex = `<div class="container-fluid text-light bg-dark">
-<a class="logoName" id="colorText" href="https://www.facebook.com/1chiton1/"
-  ><img class="transform-white" src="src/icons/logo.png" alt=""
-/></a>
-<button
-  class="navbar-toggler"
-  type="button"
-  data-bs-toggle="collapse"
-  data-bs-target="#navbarSupportedContent"
-  aria-controls="navbarSupportedContent"
-  aria-expanded="false"
-  aria-label="Toggle navigation"
->
-  <span class="navbar-toggler-icon"></span>
-</button>
-<div class="collapse navbar-collapse" id="navbarSupportedContent">
-  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-    <li class="nav-item">
-      <a
-        class="nav-link active transform-white"
-        id="colorText"
-        aria-current="page"
-        href="#"
-        >Incio</a
-      >
-    </li>
-    <li class="nav-item">
-      <a
-        class="nav-link active transform-white"
-        id="colorText"
-        aria-current="page"
-        href="./html/about.html"
-        >Acerca de</a
-      >
-    </li>
-    <li class="nav-item">
-      <a
-        class="nav-link active transform-white"
-        id="colorText"
-        aria-current="page"
-        href="./contact.html"
-        >Contacto</a
-      >
-    </li>
-    <li class="nav-item dropdown">
-      <a
-        id="colorText"
-        class="nav-link dropdown-toggle transform-white"
-        href="#"
-        role="button"
-        data-bs-toggle="dropdown"
-        aria-expanded="false"
-      >
-        Portafolio
-      </a>
-      <ul class="dropdown-menu bg-dark">
-        <li>
-          <a class="dropdown-item" id="colorText" href="#">Generation proyect</a>
-        </li>
-        <li><hr class="dropdown-divider" /></li>
-        <li>
-          <a class="dropdown-item" id="colorText" href="#">Other proyect</a>
-        </li>
-      </ul>
-    </li>
-  </ul>
-</div>
-</div>`;
-let navBarContacto = `<div class="container-fluid text-light bg-dark">
-<a class="logoName" id="colorText" href="https://www.facebook.com/1chiton1/"
-  ><img class="transform-white" src="src/icons/logo.png" alt=""
-/></a>
-<button
-  class="navbar-toggler"
-  type="button"
-  data-bs-toggle="collapse"
-  data-bs-target="#navbarSupportedContent"
-  aria-controls="navbarSupportedContent"
-  aria-expanded="false"
-  aria-label="Toggle navigation"
->
-  <span class="navbar-toggler-icon"></span>
-</button>
-<div class="collapse navbar-collapse" id="navbarSupportedContent">
-  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-    <li class="nav-item">
-      <a
-        class="nav-link active transform-white"
-        id="colorText"
-        aria-current="page"
-        href="./index.html"
-        >Incio</a
-      >
-    </li>
-    <li class="nav-item">
-      <a
-        class="nav-link active transform-white"
-        id="colorText"
-        aria-current="page"
-        href="./html/about.html"
-        >Acerca de</a
-      >
-    </li>
-    <li class="nav-item">
-      <a
-        class="nav-link active transform-white"
-        id="colorText"
-        aria-current="page"
-        href="#"
-        >Contacto</a
-      >
-    </li>
-    <li class="nav-item dropdown">
-      <a
-        id="colorText"
-        class="nav-link dropdown-toggle transform-white"
-        href="#"
-        role="button"
-        data-bs-toggle="dropdown"
-        aria-expanded="false"
-      >
-        Portafolio
-      </a>
-      <ul class="dropdown-menu bg-dark">
-        <li>
-          <a class="dropdown-item" id="colorText" href="#">Generation proyect</a>
-        </li>
-        <li><hr class="dropdown-divider" /></li>
-        <li>
-          <a class="dropdown-item" id="colorText" href="#">Other proyect</a>
-        </li>
-      </ul>
-    </li>
-  </ul>
-</div>
-</div>`;
-// Seleccionador de navbar dependiendo el html
-if ($navIndex !== null) {
-  $navIndex.insertAdjacentHTML("beforeend", navBarIndex);
-} else if ($navBarContacto !== null) {
-  $navBarContacto.insertAdjacentHTML("beforeend", navBarContacto);
-} else {
+const links = [
+  { href: "https://www.facebook.com/1chiton1/", icon: "../src/icons/logo.png" },
+  { href: "index.html", text: "Inicio" },
+  { href: "/about.html", text: "Acerca de" },
+  { href: "/contact.html", text: "Contacto" },
+];
+
+function createNavLinks() {
+  const navbar = document.getElementById("navbar");
+
+  // Crea un arreglo de elementos html link que recibe cada link de links y le imprime su valor
+  const linksHTML = links
+    .map((link, index) => {
+      if (index === 0 && link.icon) {
+        // Si es el primer enlace y tiene un icono, se agrega la imagen
+        return `
+          <li>
+            <a  href="${link.href}">
+              <img src="${link.icon}" alt="icono" class="icon transform-white">
+            </a>
+          </li>
+        `;
+      } else {
+        // En caso contrario, se agrega el texto del enlace
+        return `
+          <li>
+            <a class="transform-white" href="${link.href}">
+              ${link.text}
+            </a>
+          </li>
+        `;
+      }
+    })
+    .join("");
+
+  // Crea el navbar y su ul y le mete los links creados
+  const navHTML = `
+    <ul class="navbar-links">
+      ${linksHTML}
+    </ul>
+  `;
+
+  // Se pega en el documento
+  navbar.insertAdjacentHTML("beforeend", navHTML);
 }
-// TODO corregir el problema de que agarra
+
+createNavLinks();
---
+++ js/validaciones.js
@@ -0,0 +1,185 @@
+let txtName = document.getElementById("name");
+let txtemail = document.getElementById("txtCorreo");
+let txtMensaje = document.getElementById("mensaje");
+let txtTelefono = document.getElementById("telefono");
+const form = document.getElementById("formSend");
+console.log(form);
+
+let alertValidaciones = document.getElementById("alertValidaciones");
+let alertValidacionesTexto = document.getElementById("alertValidacionesTexto");
+
+let btnEnviar = document.getElementById("btnEnviar");
+let btnClear = document.getElementById("btnClear");
+
+let isValid = true;
+let idTimeout;
+
+function validarNombre() {
+  if (txtName.value.length < 2) {
+    return false;
+  }
+  return true;
+} //Validar Nombre
+
+function validarTelefono() {
+  if (txtTelefono.value.length == 0) {
+    return false;
+  }
+  if (isNaN(txtTelefono.value)) {
+    console.log("no es un número");
+    txtTelefono.style.border = "solid thin red";
+    alertValidacionesTexto.innerHTML += "Se debe escribir un número de 10 dígitos";
+    alertValidaciones.style.display = "block";
+    isValid = false;
+    return false;
+  }
+  if (txtTelefono.value.length > 10) {
+    console.log("es mayor a 10 dígitos");
+    txtTelefono.style.border = "solid thin red";
+    alertValidacionesTexto.innerHTML += "Se debe escribir un número de 10 dígitos";
+    alertValidaciones.style.display = "block";
+    isValid = false;
+    return false;
+  }
+
+  if (txtTelefono.value.length < 10) {
+    console.log("es menor a 10 dígitos");
+    txtTelefono.style.border = "solid thin red";
+    alertValidacionesTexto.innerHTML += "Se debe escribir un número de 10 dígitos";
+    alertValidaciones.style.display = "block";
+    isValid = false;
+    return false;
+  }
+  txtTelefono.style.border = "";
+  return true;
+} //Validar teléfono
+
+function validateEmail() {
+  // Obtener el valor del input de correo electrónico
+  const email = document.getElementById("txtCorreo");
+  //  valida el correo electrónico
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  // Validar el correo electrónico y limpia el input si es correcto
+  if (emailRegex.test(email.value)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function validarMensaje() {
+  // .replace quita los espacios
+  if (txtMensaje.value.replace(/\s+/g, "").length < 20) {
+    return false;
+    /*  mostrarMensaje('El mensaje es demasiado corto. No debe tener menos de 20 caracteres.', 'error');  */
+  } else if (txtMensaje.value.length > 160) {
+    return false;
+    /* mostrarMensaje('El mensaje es demasiado largo. No debe tener más de 160 caracteres.', 'error'); */
+  } else if (!/^[a-zA-Z0-9\s]*$/.test(txtMensaje.value)) {
+    return false;
+    /*  mostrarMensaje('El mensaje solo puede contener letras y números.', 'error'); */
+  } else {
+    return true;
+  }
+}
+
+btnEnviar.addEventListener("click", function (event) {
+  event.preventDefault();
+  isValid = true;
+  console.log("Enviando");
+  clearTimeout(idTimeout);
+
+  /* Limpiar mensaje de alerta después del click en agregar */
+  alertValidacionesTexto.innerHTML = "";
+  alertValidaciones.style.display = "none";
+
+  /* Validación de campos vacíos */
+  txtName.value = txtName.value.trim(); //* Eliminar espacios - necesita click
+
+  let lista = "Los siguientes campos deben ser llenados correctamente:<ul>";
+
+  if (!validarNombre()) {
+    txtName.style.border = "solid thin red";
+    lista += "<li>Se debe escribir un nombre válido</li>";
+    alertValidaciones.style.display = "block";
+    isValid = false;
+  } else {
+    txtName.style.border = "";
+  } // validar si el campo "Nombre" está vacio"
+
+  if (!validateEmail()) {
+    txtemail.style.border = "solid thin red";
+    lista +=
+      "<li>Se debe ingresar un correo válido que contenga '@' y un dominio; ejemplo: alguien@ejemplo.com </li>";
+    alertValidaciones.style.display = "block";
+    isValid = false;
+  } else {
+    txtemail.style.border = "";
+  } // validar campo email
+
+  if (!validarTelefono()) {
+    txtTelefono.style.border = "solid thin red";
+    lista += "<li>Se debe escribir un número de 10 dígitos</li>";
+    alertValidaciones.style.display = "block";
+    isValid = false;
+  } else {
+    txtTelefono.style.border = "";
+  } // validar campo telefono
+
+  if (!validarMensaje()) {
+    txtMensaje.style.border = "solid thin red";
+    lista +=
+      "<li>Se debe escribir un mensaje de entre 20 y 160 caracteres, sólo se permiten letras y números</li>";
+    alertValidaciones.style.display = "block";
+    isValid = false;
+  } else {
+    txtMensaje.style.border = "";
+  } // validar campo mensaje
+
+  lista += "</ul>";
+  alertValidacionesTexto.insertAdjacentHTML("beforeend", lista);
+  idTimeout = setTimeout(function () {
+    alertValidaciones.style.display = "none";
+  }, 5000); //tiempo de mensaje
+
+  //  Envia el formulario una vez se validan los campos
+  if (isValid) {
+    form.submit();
+  }
+  console.log(form);
+  //Limpia los campos cuando ya se enviaron
+  if (isValid) {
+    txtName.value = "";
+    txtTelefono.value = "";
+    txtemail.value = "";
+    txtMensaje.value = "";
+  }
+}); //btnEnviar
+
+/* Botón limpiar */
+btnClear.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  /* Limpiar campos */
+  txtName.value = "";
+  txtTelefono.value = "";
+  txtemail.value = "";
+  txtMensaje.value = "";
+}); // click btnClear
+
+/* Eliminar espacios en el inicio y fin del campo */
+txtTelefono.addEventListener("blur", function (event) {
+  event.preventDefault();
+  txtTelefono.value = txtTelefono.value.trim();
+}); /* txtTelefono.blur */
+
+txtName.addEventListener("blur", function (event) {
+  event.preventDefault();
+  txtName.value = txtName.value.trim();
+}); /* txtName.blur */
+
+txtemail.addEventListener("blur", function (event) {
+  event.preventDefault();
+  txtemail.value = txtemail.value.trim();
+}); /* txtMail.blur */
